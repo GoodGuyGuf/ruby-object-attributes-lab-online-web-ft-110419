0e7624bf3b0ff3f5406d55deaa31aa63397d1539
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-13 01:43:12 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,24 +1,6 @@
 class Person
 
-<<<<<<< HEAD
   attr_accessor :name
   attr_accessor :job
-
-=======
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
-
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
-  end
-
-  def name
-    "#{@first_name} #{@last_name}".strip
-  end
->>>>>>> add2faff6dca683b8ba7fbb2f28bb444d0ca6b91
 
 end
