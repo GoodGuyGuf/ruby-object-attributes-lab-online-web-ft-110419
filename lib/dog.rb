389a8cde6c124c71235fe38e239b8811d5aa665a
Ethan Gustafson <ethan_gustafson@outlook.com>
2019-11-13 01:42:37 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,23 @@
 class Dog
 
+<<<<<<< HEAD
 attr_accessor :name
 attr_accessor :breed
+=======
+  def initialize(name, breed)
+  @name = name
+  @breed = breed
+  end
+
+  def name=(dogs_data)
+  name, breed = dogs_data.split
+  @name = name
+  @breed = breed
+  end
+
+  def name
+  "#{@name} #{@breed}".strip
+  end
+
+>>>>>>> add2faff6dca683b8ba7fbb2f28bb444d0ca6b91
 end
